94091799ef984dd51f605193a0be168f0d37b0b9
Paulo Vasconcelos <paulobvasconcelos@gmail.com>
2018-03-01 11:07:03 +0000

    [AlgC] Prática 02

---
+++ Ano_2/AlgC/Práticas/Aula02/form1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (int argc, char* argv[])
+{
+    int n = atoi(argv[1]);
+    int result = 0;
+    int nAdd = 0;
+    if(!(n==0 || n==1)) {
+        
+    }
+    printf("Result = %d\n",r);
+    printf("Sums = %d\n",nAdd);
+    return r;
+}
